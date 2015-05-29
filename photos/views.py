bf2d613f17b69b919269283a1f617af82fb0d88e
pabloparejo <parejo.p@gmail.com>
2015-05-29 17:21:02 +0200

    home view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    html = '<strong>Hello world!</strong>'
+    return HttpResponse(html)
