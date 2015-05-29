7b9098eda88b682491ee5f11b3d6af70f687531d
pabloparejo <parejo.p@gmail.com>
2015-05-29 18:56:45 +0200

    first template

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from photos.models import Photo
 # Create your views here.
 
 
 def home(request):
-    html = '<strong>Hello world!</strong>'
-    return HttpResponse(html)
+    photos = Photo.objects.all()
+    context = {"photos": photos}
+    return render(request, "photos/home.html", context)
