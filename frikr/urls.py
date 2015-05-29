0845b028ca82fef65481a3f622c13774c1b97c07
pabloparejo <parejo.p@gmail.com>
2015-05-29 20:40:42 +0200

    urls name

--- frikr/urls.py
+++ frikr/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'photos.views.home'),
-    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.photo_detail', name="photo_detail"),
+    url(r'^$', 'photos.views.home', name="home"),
+    url(r'^login$', 'photos.views.home', name="login"),
+    url(r'^logout$', 'photos.views.home', name="logout"),
+    url(r'^photos/(?P<pk>[0-9]+)-(.*)/$', 'photos.views.photo_detail', name="photo_detail"),
 ]
