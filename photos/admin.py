c9d11c31536182e28eed82cf39e297fd861dada6
pabloparejo <parejo.p@gmail.com>
2015-06-11 21:12:48 +0200

    custom admin

--- photos/admin.py
+++ photos/admin.py
@@ -2,4 +2,27 @@ from django.contrib import admin
 from photos.models import Photo
 # Register your models here.
 
-admin.site.register(Photo)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner_name', 'license', 'visibility')
+    list_filter = ('license', 'visibility', 'created_on')
+    search_fields = ('name', 'description', 'owner_name')
+
+    fieldsets = (
+        ("Photo", {
+            "classes": ("wide",),
+            "fields": ("name", 'owner', 'url', 'description',)
+        }),
+        ("License & visibility", {
+            "classes": ("wide", "collapse"),
+            "fields": ('license','visibility')
+        })
+    )
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + ' ' + obj.owner.last_name
+
+    owner_name.short_description = "Author"
+    owner_name.admin_order_field = "owner__first_name"
+
+admin.site.register(Photo, PhotoAdmin)
